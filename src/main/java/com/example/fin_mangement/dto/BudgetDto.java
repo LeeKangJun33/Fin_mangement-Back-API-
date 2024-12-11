3135413601f40c2205819f8c69a0a90815ceb026
LeeKangJun33 <drfting1230@naver.com>
2024-12-11 16:47:19 +0900

    fix(Budget): DTO 수정 ADD : Jsonproperty 추가

--- src/main/java/com/example/fin_mangement/dto/BudgetDto.java
+++ src/main/java/com/example/fin_mangement/dto/BudgetDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class BudgetDto {
 
-    @JsonProperty("budgetName")
+   @JsonProperty("name")
     private String budgetName;
     @JsonProperty("amount")
     private double amount;
